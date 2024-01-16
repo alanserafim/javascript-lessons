90fc560a2993f73785033e4b07ae2da25d843dd2
alanserafim <alan.serafim@fatec.sp.gov.br>
2024-01-16 16:40:35 -0300

    dia 01 desafio js

---
+++ js_desafio_30-days-of-javascript/desafio/dia_01_introducao/01.js
@@ -0,0 +1,28 @@
+console.log("Dia 01");
+
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, 
+easy to reuse 
+and informative
+*/
+
+let varString = "string"
+console.log( typeof varString);
+
+let varBoolean = false
+console.log( typeof varBoolean);
+
+let varUndefined = undefined
+console.log( typeof varUndefined);
+
+let varNull = null
+console.log( typeof varNull);
+
+let varFloat = 12.001
+console.log( typeof varFloat);
+
+let varInteger = 10
+console.log( typeof varInteger);
