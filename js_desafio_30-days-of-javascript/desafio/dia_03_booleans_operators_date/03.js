d4b32c4d47181e7602f8f75fc22f0e55347d138d
alanserafim <alan.serafim@fatec.sp.gov.br>
2024-01-20 20:36:37 -0300

    dia 03 finalizado

--- js_desafio_30-days-of-javascript/desafio/dia_03_booleans_operators_date/03.js
+++ js_desafio_30-days-of-javascript/desafio/dia_03_booleans_operators_date/03.js
@@ -1,10 +1,9 @@
-const now = new Date()
-console.log(now)
+// area of the rectangle
 
-console.log("year: ", now.getFullYear()) // 2020
-console.log("month: ", now.getMonth()) // 0, because the month is January,  month(0-11)
-console.log("date/day of the month: ", now.getDate()) // 4, because the day of the month is 4th,  day(1-31)
-console.log("day of the week", now.getDay())
-console.log("hours: ", now.getHours())
-console.log("minutes: ", now.getMinutes()) // 56, because the time is 00:56:41
-console.log("seconds: ", now.getSeconds()) // 41, because the time is 00:56:41
+let RetLenght = window.prompt("length")
+let width = window.prompt("width")
+let area = RetLenght * width
+let perimeter =  2 * (RetLenght + width)
+
+
+console.log(`The area of the retangle is ${area} and the perimeter is ${peri}`)
--- js_desafio_30-days-of-javascript/main.js
+++ js_desafio_30-days-of-javascript/main.js
@@ -1,1 +1,15 @@
-console.log("Olá, Mundo")
+let now = new Date()
+let day = now.getDate()
+let month = now.getMonth() <10 ? `0${now.getMonth() + 1}` : now.getMonth() + 1
+let year = now.getFullYear()
+let hours = now.getHours().toFixed(0)
+let minutes = now.getMinutes().toFixed(0)
+
+let date1 = `${year}/${month}/${day} ${hours}:${minutes}`
+let date2 = `${day}-${month}-${year} ${hours}:${minutes}`
+let date3 = `${day}/${month}/${year} ${hours}:${minutes}`
+
+
+console.log(date1);
+console.log(date2);
+console.log(date3);
